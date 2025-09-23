c541aa00437742d5c3160bd942465af0a21b1c8d
Marko Petzold <marko.petzold@record-evolution.de>
2025-09-23 16:23:21 +0200

    fix: update navigation item click handler to use inputData directly

--- src/widget-sidenav.ts
+++ src/widget-sidenav.ts
@@ -145,7 +145,7 @@ export class WidgetSidenav extends LitElement {
                     class="paging"
                     style=${this.inputData?.route ? 'cursor: pointer' : ''}
                     ?active=${this.inputData?.title}
-                    @click=${() => this.handleNavItemClick(this.addSlashes(this.inputData?.route))}
+                    @click=${() => this.handleNavItemClick(this.addSlashes(this.inputData))}
                 >
                     ${this.inputData?.title}
                 </h2>
